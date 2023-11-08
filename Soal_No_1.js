eb729802b4b70da1b371dadbddd0fa21dd5854ce
IrsanJam <irsandjamaludin@gmail.com>
2023-11-09 06:20:43 +0700

    membuat perbaikan di 2 flowchart

--- Soal_No_1.js
+++ Soal_No_1.js
@@ -1,11 +1,11 @@
-function cekFaktorBilangan(angka) {
+function cekFaktorBilangan(n) {
   let faktor = 1;
-  for (let i = 1; i < angka; i++) {
-    if (angka % i === 0) {
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
       faktor++;
     }
   }
   faktor > 4 ? console.log("Bukan Bilangan Keren") : console.log("Bilangan Keren");
 }
 
-cekFaktorBilangan(52);
+cekFaktorBilangan(51);
--- Soal_No_2.js
+++ Soal_No_2.js
@@ -1,7 +1,7 @@
-function cekKondisiLampu(angka) {
+function cekKondisiLampu(n) {
   let faktor = 1;
-  for (let i = 1; i < angka; i++) {
-    if (angka % i === 0) {
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
       faktor++;
     }
   }
